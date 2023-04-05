cc3cd2a82029c26e03114db2cdd16fa78b2e52fd
Aayushi-4094 <aayushiagarwal580@gmail.com>
2023-04-05 15:34:27 +0530

    Code fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import AddTaskForm from './components/AddTaskForm.jsx';
 import UpdateForm from './components/UpdateForm.jsx';
 import ToDo from './components/ToDo.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
